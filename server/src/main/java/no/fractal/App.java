485d42c833fcb9266af4b2af75c1ef3770ef7ede
Christoffer A Traeen <christoffer.andersen@live.com>
2020-09-11 14:17:03 +0200

    Set logger style

--- server/src/main/java/no/fractal/App.java
+++ server/src/main/java/no/fractal/App.java
@@ -4,10 +4,13 @@ package no.fractal;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    /*
+     * Change the format of the java util logger to single line message in format:
+     * [Data time] [type] message @ class
+     */
+    static {
+        System.setProperty("java.util.logging.SimpleFormatter.format",
+                "[%1$tb %1$td %1$tY] [%1$tT] %2$s%n%4$s: %5$s%n");
+    }
     }
 }
