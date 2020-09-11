60fa19966845eeed615655ad7a3afcf4ec43bf1b
Christoffer A Traeen <christoffer.andersen@live.com>
2020-09-11 14:17:10 +0200

    Start tcp

--- server/src/main/java/no/fractal/App.java
+++ server/src/main/java/no/fractal/App.java
@@ -1,9 +1,13 @@
 package no.fractal;
 
+import no.fractal.socket.TcpServer;
+
 /**
  * Hello world!
  *
  */
+public class App {
+
     /*
      * Change the format of the java util logger to single line message in format:
      * [Data time] [type] message @ class
@@ -12,5 +16,9 @@ package no.fractal;
         System.setProperty("java.util.logging.SimpleFormatter.format",
                 "[%1$tb %1$td %1$tY] [%1$tT] %2$s%n%4$s: %5$s%n");
     }
+
+    public static void main(String[] args) {
+        TcpServer sr = new TcpServer();
+        sr.start();
     }
 }
