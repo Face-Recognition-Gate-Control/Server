f813a577a0fe6fb7033014b65a2ae869b0ac45fb
trygve <trygve.woldseth@gmail.com>
2020-09-25 19:39:57 +0200

    response sys

--- server/src/main/java/no/fractal/socket/send/AbstractMessage.java
+++ server/src/main/java/no/fractal/socket/send/AbstractMessage.java
@@ -1,7 +1,150 @@
 package no.fractal.socket.send;
 
+import no.fractal.socket.StreamUtil;
+import no.fractal.util.StreamUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class AbstractMessage {
 
-    abstract int getBo
+    private final String messageType;
+    private final ArrayList<Segment> segments;
+
+
+    protected AbstractMessage(String messageType
+    ) {
+        this.messageType = messageType;
+        this.segments = new ArrayList<>();
+    }
+
+    protected void addSegment(Segment segment){
+        this.segments.add(segment);
+    }
+
+
+    public void writeToStream(BufferedOutputStream outputStream) throws IOException{
+
+        byte[] typeBytes = this.messageType.getBytes();
+        byte[] segmentBytes = getMainSegmentMeta().toString().getBytes();
+
+        // byte count
+        int byteCont = segments.stream().map(Segment::getBodyByteSize).reduce(Integer::sum).get();
+        byteCont += segmentBytes.length;
+        byteCont += typeBytes.length;
+        byteCont += 14; // hederstuff
+
+        // - payload tot size 4B
+        StreamUtils.writeInt(outputStream, byteCont);
+
+        // - id Size 2b and id
+        StreamUtils.writeInt(outputStream, typeBytes.length);
+        outputStream.write(typeBytes);
+
+        // - segmentArr size 2b and arr
+        StreamUtils.writeInt(outputStream, segmentBytes.length);
+        outputStream.write(segmentBytes);
+
+        for (Segment segment: segments){
+            segment.writeToStream(outputStream);
+        }
+    }
+
+    private JSONArray getMainSegmentMeta(){
+        JSONArray ret = new JSONArray();
+        segments.forEach(segment -> ret.put(segment.segmentMeta));
+        return ret;
+    }
+
+    private abstract static class Segment{
+        protected final Map<String,String> segmentMeta;
+
+        protected Segment(Map<String, String> segmentMeta) {
+            this.segmentMeta = segmentMeta;
+        }
+
+        Map<String, String> getSegmentMeta() {
+            return segmentMeta;
+        }
+
+        public int getBodyByteSize(){
+            return Integer.parseInt(segmentMeta.get(keys.size.name()));
+        }
+
+        public abstract void writeToStream(BufferedOutputStream outputStream) throws IOException;
+
+        protected enum keys{
+            size,
+            mime_type,
+            file_name
+        }
+    }
+
+    public static class FileSegment extends Segment{
+        public FileSegment(File file)  {
+            super(new HashMap<>());
+            this.file = file;
+
+            try {
+                this.segmentMeta.put(keys.size.name(), String.valueOf(file.length()));
+                this.segmentMeta.put(keys.mime_type.name(), String.valueOf(Files.probeContentType(file.toPath())));
+                this.segmentMeta.put(keys.file_name.name(), file.getName());
+
+            } catch (Exception e){ e.printStackTrace();}
+
+        }
+
+        private final File file;
+
+        @Override
+        public void writeToStream(BufferedOutputStream outputStream) throws IOException {
+            StreamUtils.writeFileToStream(outputStream,file);
+        }
+    }
+
+    public static class JsonSegment extends Segment{
+        private final byte[] bytes;
+
+        public JsonSegment(JSONObject jsonObject) {
+            super(new HashMap<>());
+            this.bytes = jsonObject.toString().getBytes();
+
+            this.segmentMeta.put(keys.size.name(), String.valueOf(bytes.length));
+            this.segmentMeta.put(keys.mime_type.name(), "application/json");
+
+        }
+
+        @Override
+        public void writeToStream(BufferedOutputStream outputStream) throws IOException {
+            outputStream.write(bytes);
+        }
+    }
+
+
+    public static class ByteSegment extends Segment{
+        private final byte[] blob;
+
+        public ByteSegment(byte[] blob) {
+            super(new HashMap<>());
+            this.blob = blob;
+
+            this.segmentMeta.put(keys.size.name(), String.valueOf(blob.length));
+            this.segmentMeta.put(keys.mime_type.name(), "application/bytes");
+        }
+
+
+        @Override
+        public void writeToStream(BufferedOutputStream outputStream) throws IOException {
+            outputStream.write(blob);
+        }
+    }
+
+
 
 }
--- server/src/main/java/no/fractal/socket/send/MessageDispatcher.java
+++ server/src/main/java/no/fractal/socket/send/MessageDispatcher.java
@@ -1,5 +1,7 @@
 package no.fractal.socket.send;
 
+import no.fractal.util.StreamUtils;
+
 import java.io.BufferedOutputStream;
 import java.lang.annotation.Target;
 import java.util.Vector;
@@ -9,11 +11,16 @@ public class MessageDispatcher {
     private Vector<AbstractMessage> messageQue;
     private BufferedOutputStream outputStream;
 
+    private Thread dispatchThread;
+
     public MessageDispatcher(BufferedOutputStream bufferedOutputStream) {
         this.outputStream = bufferedOutputStream;
+        this.dispatchThread = new Thread(this::run);
+        dispatchThread.start();
     }
 
-    public void addTask(AbstractMessage message){
+
+    public void addMessage(AbstractMessage message){
         messageQue.add(message);
         this.notifyAll();
     }
@@ -23,9 +30,8 @@ public class MessageDispatcher {
             try {
                 if (messageQue.isEmpty()) wait();
 
-                AbstractMessage messageMessage =  messageQue.remove(0);
-
-
+                AbstractMessage message =  messageQue.remove(0);
+                message.writeToStream(this.outputStream);
 
             } catch (Exception e){
                 e.printStackTrace();
