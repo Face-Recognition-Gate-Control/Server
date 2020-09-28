dd30f3d02f30178acb7128cfee3c42b8c9c1280b
trygve <trygve.woldseth@gmail.com>
2020-09-28 17:53:08 +0200

    compose fixes to docker

--- docker_files/db_init/2_initDB.sql
+++ docker_files/db_init/2_initDB.sql
@@ -7,16 +7,16 @@ CREATE TABLE IF NOT EXISTS users(
     email      text                not null,
     telephone  number              not null,
     password   text                not null,
-    created    bigint DEFAULT extract(epoch from now()),
+    created    bigint DEFAULT extract(epoch from now())
 
-)
+);
 
 CREATE TABLE IF NOT EXISTS stations(
     id             uuid primary key    not null,
     station_name   text                not null,
     last_chekin    bigint              not null,
 
-)
+);
 
 CREATE TABLE IF NOT EXISTS events(
     id             serial int primary key,
@@ -24,40 +24,40 @@ CREATE TABLE IF NOT EXISTS events(
     event_start    bigint              not null,
     event_end      bigint              not null,
     event_day      bigint              not null,
-    repets         boolean             not null,
+    repets         boolean             not null
 
-)
+);
 CREATE TABLE IF NOT EXISTS station_events(
     text references stations(id) ON DELETE RESTRICT  not null,
-    text references events(id) ON DELETE RESTRICT  not null,
+    text references events(id) ON DELETE RESTRICT  not null
 
-)
+);
 
 
 CREATE TABLE IF NOT EXISTS roles(
-    role_name      text      primary key  not null,
-)
+    role_name      text      primary key  not null
+);
 
 CREATE TABLE IF NOT EXISTS user_roles(
     role_name      text references roles(role_name) ON DELETE RESTRICT  not null,
-    user_id        text references users(id)        ON DELETE CASCADE   not null,
+    user_id        text references users(id)        ON DELETE CASCADE   not null
 )
 
 CREATE TABLE IF NOT EXISTS thumbnails(
     user_id        text references users(id)        ON DELETE CASCADE   not null,
-    file_name      text                                                 not null,
+    file_name      text                                                 not null
 )
 
 CREATE TABLE IF NOT EXISTS login_referance(
     user_id        uuid references users(id)        ON DELETE CASCADE   not null,
     face_vec       numeric[]                                            not null,
-    file_name      text                                                 not null,
+    file_name      text                                                 not null
 )
 
 CREATE TABLE IF NOT EXISTS registrations(
     token          uuid primary key    not null,
     station_name   text                not null,
-    expire         bigint              not null,
+    expire         bigint              not null
 
 )
 
