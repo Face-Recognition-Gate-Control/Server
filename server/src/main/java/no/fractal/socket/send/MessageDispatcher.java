c3fda3d561c5ced111a89d21117922df9c8ba507
Christoffer A Traeen <christoffer.andersen@live.com>
2020-09-28 20:34:38 +0200

    Fix message dispatcher

--- server/src/main/java/no/fractal/socket/send/MessageDispatcher.java
+++ server/src/main/java/no/fractal/socket/send/MessageDispatcher.java
@@ -3,42 +3,73 @@ package no.fractal.socket.send;
 import java.io.BufferedOutputStream;
 import java.util.Vector;
 
+/**
+ * Message dispatcher is responsible for delivering messages/payloads to a
+ * outputstream provided to the dispatcher. The dispatcher is running on its own
+ * thread, so it will not block while sending data. Messages are added to a
+ * queue, and each message is written to the stream in FIFO order. Adding a
+ * message will wake the thread if it is asleep and it will start stream the
+ * message.
+ */
 public class MessageDispatcher {
 
-    private Vector<AbstractMessage> messageQueue;
+    /**
+     * Message queue for messages to be sent
+     */
+    private Vector<AbstractMessage> messageQueue = new Vector<AbstractMessage>();;
+
+    /**
+     * The stream it will write to
+     */
     private BufferedOutputStream outputStream;
 
+    /**
+     * Thread the dispatcher is running on
+     */
     private Thread dispatchThread;
 
+    /**
+     * Binds the outputstream and start the dispatchet thread.
+     * 
+     * @param bufferedOutputStream the stream we want to transfer to
+     */
     public MessageDispatcher(BufferedOutputStream bufferedOutputStream) {
+
         this.outputStream = bufferedOutputStream;
+
         this.dispatchThread = new Thread(this::run);
         dispatchThread.start();
     }
 
-
-    public void addMessage(AbstractMessage message){
+    /**
+     * Adds a message to the queue, it wakes the thread when message is added.
+     * 
+     * @param message the message to send to the client
+     */
+    public synchronized void addMessage(AbstractMessage message) {
         messageQueue.add(message);
         this.notifyAll();
     }
 
-    private void run(){
-        while (true){
+    /**
+     * Infinite loops over message queue, and streams the messages while awake. if
+     * the queue is empty, the thread waits.
+     */
+    private synchronized void run() {
+        while (true) {
             try {
-                if (messageQueue.isEmpty()) wait();
+                if (messageQueue.isEmpty()) {
+                    wait();
+                }
 
-                AbstractMessage message =  messageQueue.remove(0);
+                AbstractMessage message = messageQueue.remove(0);
                 message.writeToStream(this.outputStream);
 
-            } catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
 
-
-
         }
     }
-
-
 
 }
