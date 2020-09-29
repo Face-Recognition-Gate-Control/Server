ce13dad3b3c578d8e721c249796d75feb88ded04
trygve <trygve.woldseth@gmail.com>
2020-09-29 12:46:23 +0200

    added whatcher for changes to table

--- docker_files/db_init/2_initDB.sql
+++ docker_files/db_init/2_initDB.sql
@@ -1,5 +1,28 @@
 \c fractal
 
+CREATE TABLE IF NOT EXISTS updates_table(
+   id             text primary key    not null,
+   last_change    bigint              not null
+);
+
+
+CREATE FUNCTION watch_updates(tbl text) RETURNS void AS $$
+    BEGIN
+        INSERT INTO
+            updates_table (id, last_change)
+            VALUES (tbl, extract(epoch from now()))
+            ON CONFLICT (id)
+            DO
+                UPDATE SET (last_change) = extract(epoch from now());
+    END;
+    $$ LANGUAGE plpgsql;
+
+CREATE FUNCTION watch_user_data() RETURNS TRIGGER AS $$
+    BEGIN
+    EXECUTE watch_updates('new_user_queue');
+    END;
+    $$ LANGUAGE plpgsql;
+
 -- table with users
 CREATE TABLE IF NOT EXISTS users(
     id         uuid primary key    not null,
@@ -23,10 +46,9 @@ CREATE TABLE IF NOT EXISTS stations(
 
 -- when what user entered what station
 CREATE TABLE IF NOT EXISTS user_enter_events(
-   user_id        uuid references stations(id)   not null,
+   user_id        uuid references users(id)   not null,
    station_id     uuid references stations(id)   not null,
    enter_time     bigint DEFAULT extract(epoch from now())
-
 );
 
 -- the timetable for stuff this may change per frontend result
@@ -68,6 +90,7 @@ CREATE TABLE IF NOT EXISTS login_referance(
     file_name      text                                                 not null
 );
 
+
 CREATE TABLE IF NOT EXISTS new_user_queue(
     tmp_id         uuid        not null,
     face_vec       numeric[]   not null,
@@ -82,6 +105,12 @@ CREATE TABLE IF NOT EXISTS registrations(
     expire         bigint              not null
 
 );
+
+
+CREATE TRIGGER watch_user_data
+    AFTER INSERT OR DELETE OR UPDATE
+    ON login_referance
+    EXECUTE FUNCTION watch_user_data();
 
 
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO fractal;
