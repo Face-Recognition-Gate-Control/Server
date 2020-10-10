f36acc468ce6de3fae9adcf9efce9bae60b3aae3
Christoffer A Traeen <christoffer.andersen@live.com>
2020-10-10 14:14:30 +0200

    Move dispatcher creation

--- server/src/main/java/no/fractal/socket/FractalClient.java
+++ server/src/main/java/no/fractal/socket/FractalClient.java
@@ -1,122 +1,107 @@
 package no.fractal.socket;
 
-import no.fractal.socket.meta.*;
-
-import no.fractal.socket.messages.recive.GateAuthorizationPayload;
-import no.fractal.socket.messages.recive.PingPayload;
-import no.fractal.socket.messages.recive.UserAuthorizationPayload;
-import no.fractal.socket.messages.recive.UserEnteredPayload;
-import no.fractal.socket.payload.InvalidPayloadException;
-import no.fractal.socket.messages.recive.UserThumbnailPayload;
-
+import com.google.gson.JsonSyntaxException;
 import no.fractal.socket.messages.recive.NoSuchPayloadException;
 import no.fractal.socket.messages.recive.PayloadBase;
+import no.fractal.socket.payload.InvalidPayloadException;
+import no.fractal.socket.send.MessageDispatcher;
 
 import java.io.BufferedInputStream;
-
 import java.io.IOException;
-
 import java.net.Socket;
 import java.net.SocketException;
-
 import java.util.UUID;
-import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.google.gson.JsonSyntaxException;
-import no.fractal.socket.send.MessageDispatcher;
-
 public class FractalClient extends Client {
 
-	private MessageDispatcher dispatcher;
-	private UUID GateId;
+    private MessageDispatcher dispatcher;
+    private UUID              GateId;
 
-	private Function<FractalProtocol.PayloadData, PayloadBase> payloadBuilder = payloadData -> null;
+    private Function<FractalProtocol.PayloadData, PayloadBase> payloadBuilder = payloadData -> null;
 
-	public UUID getGateId() {
-		return GateId;
-	}
+    public UUID getGateId() {
+        return GateId;
+    }
 
-	public void setGateId(UUID gateId) {
-		this.GateId = gateId;
-	}
+    public void setGateId(UUID gateId) {
+        this.GateId = gateId;
+    }
 
-	// Handles logging for the FractalClient
-	private static Logger LOGGER = Logger.getLogger(FractalClient.class.getName());
+    // Handles logging for the FractalClient
+    private static Logger LOGGER = Logger.getLogger(FractalClient.class.getName());
 
-	private FractalProtocol protocol = new FractalProtocol();
+    private FractalProtocol protocol = new FractalProtocol();
 
-	public FractalClient(Socket clientSocket, TcpServer server) throws IOException {
-		super(clientSocket, server);
-	}
+    public FractalClient(Socket clientSocket, TcpServer server) throws IOException {
+        super(clientSocket, server);
+    }
 
-	public void setPayloadBuilder(Function<FractalProtocol.PayloadData, PayloadBase> payloadBuilder) {
-		this.payloadBuilder = payloadBuilder;
-	}
+    public void setPayloadBuilder(Function<FractalProtocol.PayloadData, PayloadBase> payloadBuilder) {
+        this.payloadBuilder = payloadBuilder;
+    }
 
-	@Override
-	public void run() {
-		this.dispatcher = new MessageDispatcher(this.getOutputStream());
-		this.read();
-	}
+    @Override
+    public void run() {
+        this.read();
+    }
 
-	/**
-	 * Reads all incoming headers and routes the payloads to the approporiate
-	 * payload handlers.
-	 */
-	@Override
-	protected void read() {
+    /**
+     * Reads all incoming headers and routes the payloads to the approporiate
+     * payload handlers.
+     */
+    @Override
+    protected void read() {
+        try {
 
-		try {
+            BufferedInputStream in = this.getInputReader();
+            this.dispatcher = new MessageDispatcher(this.getOutputStream());
 
-			BufferedInputStream in = this.getInputReader();
+            boolean reading = true;
+            while (reading) {
 
-			boolean reading = true;
-			while (reading) {
-
-				// Blocks here until all header fields are red.
-				FractalProtocol.PayloadData payloadData = protocol.readPayloadData(in);
+                // Blocks here until all header fields are red.
+                FractalProtocol.PayloadData payloadData = protocol.readPayloadData(in);
 
 
-				try {
-					PayloadBase payload = payloadBuilder.apply(payloadData);
+                try {
+                    PayloadBase payload = payloadBuilder.apply(payloadData);
 
-					if (payload == null) {
-						throw new NoSuchPayloadException("Can not find the payload with name: " + payloadData.getId());
-					}
-					// Execute the payload
-					payload.setDispatcher(dispatcher);
-					payload.setClient(this);
-					payload.execute();
-				} catch (JsonSyntaxException e) {
-					// SEND INVALID META FOR PAYLOAD
-					LOGGER.log(Level.INFO, String.format("Invalid meta for: %s", payloadData.getId()));
-				} catch (NoSuchPayloadException | InvalidPayloadException e) {
-					// SEND INVALID PAYLOAD NAME
-					LOGGER.log(Level.INFO, String.format("%s", e.getMessage()));
-				}
+                    if (payload == null) {
+                        throw new NoSuchPayloadException("Can not find the payload with name: " + payloadData.getId());
+                    }
+                    // Execute the payload
+                    payload.setDispatcher(dispatcher);
+                    payload.setClient(this);
+                    payload.execute();
+                } catch (JsonSyntaxException e) {
+                    // SEND INVALID META FOR PAYLOAD
+                    LOGGER.log(Level.INFO, String.format("Invalid meta for: %s", payloadData.getId()));
+                } catch (NoSuchPayloadException | InvalidPayloadException e) {
+                    // SEND INVALID PAYLOAD NAME
+                    LOGGER.log(Level.INFO, String.format("%s", e.getMessage()));
+                }
 
-				/**
-				 * Make sure all data for payload is cleared.
-				 */
-				protocol.clearStream(in);
-			}
+                /**
+                 * Make sure all data for payload is cleared.
+                 */
+                protocol.clearStream(in);
+            }
 
-			this.getClientSocket().close();
-			LOGGER.log(Level.INFO, String.format("Client disconnected: %s", this.getClientID()));
-		} catch (SocketException e) {
-			LOGGER.log(Level.SEVERE, e.getMessage());
-		} catch (IOException e) {
-			LOGGER.log(Level.SEVERE, e.getMessage());
-		} catch (Exception e) {
-			e.printStackTrace();
-			LOGGER.log(Level.SEVERE, e.getMessage());
-		}
+            this.getClientSocket().close();
+            LOGGER.log(Level.INFO, String.format("Client disconnected: %s", this.getClientID()));
+        } catch (SocketException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage());
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, e.getMessage());
+        }
 
-	}
-
+    }
 
 
 }
