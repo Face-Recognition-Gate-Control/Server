7a592ec58812180dd7598dd0709daf1c816ccf3d
freshfish70 <christoffer.andersen@live.com>
2020-10-12 15:30:28 +0200

    Create debug logger

--- web/api/src/loaders/logger.ts
+++ web/api/src/loaders/logger.ts
@@ -1,11 +1,45 @@
+import Logger from 'bunyan'
 import bunyan from 'bunyan'
 
 const loggerName = 'STDLOGGER'
 /**
  * Instantiate a new bunyan logger
  */
-const Logger = bunyan.createLogger({
+const BunyanLogger = bunyan.createLogger({
     name: loggerName,
     stream: process.stdout,
 })
-export default Logger
+
+class ConsoleLogger {
+    display(any?: any, message?: any) {
+        console.log(message)
+
+        console.log('====================================================\n')
+        if (message) {
+            console.log(message)
+            console.log('____________________________________________________')
+        }
+        if (any) console.log(any)
+        console.log('====================================================\n')
+    }
+
+    public info(any?: any, message?: any) {
+        this.display(any, message)
+    }
+
+    public warn(any?: any, message?: any) {
+        this.display(any, message)
+    }
+
+    public error(any?: any, message?: any) {
+        console.log(message)
+
+        this.display(any, message)
+    }
+
+    public fatal(any?: any, message?: any) {
+        this.display(any, message)
+    }
+}
+const Logger2 = new ConsoleLogger()
+export default Logger2
