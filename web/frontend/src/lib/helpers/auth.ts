818f9a48c6d998f745c9d03990406e71c1ed807d
Christoffer A Traeen <christoffer.andersen@live.com>
2020-10-29 14:13:37 +0100

    Login register

---
+++ web/frontend/src/lib/helpers/auth.ts
@@ -0,0 +1,54 @@
+// inspirations from https://chrismroberts.com/2019/01/03/authentication-and-protected-routes-in-vuejs/
+import jwtdecoder from 'jwt-decode'
+import { gqlAxios } from './axios'
+
+const tokenKey = 'jwt'
+
+export function setToken(token: string) {
+  window.sessionStorage.setItem(tokenKey, token)
+  gqlAxios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
+
+export function getToken() {
+  return window.sessionStorage.getItem(tokenKey)
+}
+
+export function isLoggedIn() {
+  const token = getToken()
+
+  if (token) {
+    return !isTokenExpired(token)
+  } else {
+    return false
+  }
+}
+
+export function logout() {
+  clearToken()
+}
+
+function clearToken() {
+  gqlAxios.defaults.headers.common['Authorization'] = ``
+  window.sessionStorage.removeItem(tokenKey)
+}
+
+function isTokenExpired(token: string) {
+  let expirationDate = getTokenExpirationDate(token)
+  if (expirationDate) {
+    return expirationDate < new Date()
+  }
+  return false
+}
+
+function getTokenExpirationDate(token: string) {
+  let decoded: any = jwtdecoder(token)
+
+  if (!decoded.exp) {
+    return null
+  }
+
+  let date = new Date(0)
+  date.setUTCSeconds(decoded.exp)
+
+  return date
+}
--- web/frontend/src/router/index.ts
+++ web/frontend/src/router/index.ts
@@ -1,3 +1,4 @@
+import { isLoggedIn } from '@/lib/helpers/auth'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
@@ -14,12 +15,41 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  },
+  {
+    path: '/user',
+    name: 'User page',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Userpage.vue'),
+    meta: {
+      protected: true
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach((to, _, next) => {
+  console.log(isLoggedIn())
+
+  if (to.meta.protected && !isLoggedIn()) {
+    next({ path: '/login' })
+  } else {
+    next()
+  }
 })
 
 export default router
