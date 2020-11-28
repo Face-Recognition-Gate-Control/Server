45d48603423a7d64202f42c4071f666c1e056bb0
TrygveJW <48008160+TrygveJW@users.noreply.github.com>
2020-11-28 20:22:05 +0100

    Update and rename start_master.sh to start_server.sh

--- start_master.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-#mvn -f Docker_manager/ clean compile package shade:shade && mvn -f ServerApi/ clean compile package &&
-docker-compose -f ./docker-compose.yml --project-directory ./ --env-file ./config_files/main.env up --build -V
